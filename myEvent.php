e1a4623cc19ecd443076482e7aae25c9d221548d
shirokasunawa <61684977+shirokasunawa@users.noreply.github.com>
2021-07-06 18:58:59 +0200

    event prepare

--- myEvent.php
+++ myEvent.php
@@ -12,50 +12,21 @@
   }
   </script>
  
-   <span id="typeEvents"></span>
+   
    <div class="container-fluid">
       <div class="row">
         <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-          <div class="sidebar-sticky">
-            <ul class="nav flex-column">
-              <li class="nav-item">
-                <a class="nav-link active" href="#">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
-                 Mon tableau de bord
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>
-                  Orders
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
-                  Products
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
-                  Customers
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bar-chart-2"><line x1="18" y1="20" x2="18" y2="10"></line><line x1="12" y1="20" x2="12" y2="4"></line><line x1="6" y1="20" x2="6" y2="14"></line></svg>
-                  Reports
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-layers"><polygon points="12 2 2 7 12 12 22 7 12 2"></polygon><polyline points="2 17 12 22 22 17"></polyline><polyline points="2 12 12 17 22 12"></polyline></svg>
-                  Integrations
-                </a>
-              </li>
-            </ul>
-
+          <div class="sidebar-sticky" >
+          
+            <div class=" align-content-stretch">
+              <span id="infoTitre"></span>
+              
+              <span id="infoBudget"></span>
+              <span id="infoTypeEvent"></span>
+              
+              <span id="infoDate"></span>
+              
+            </div>
             <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
               <span>Vos Actions</span>
               <a class="d-flex align-items-center text-muted" href="#">
@@ -93,24 +64,26 @@
 
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4"><div class="chartjs-size-monitor" style="position: absolute; inset: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
           <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-            <h1 class="h2">Mon tableau de bord</h1>
+             <span id="typeEvents"></span>
             <div class="btn-toolbar mb-2 mb-md-0">
               <div class="btn-group mr-2">
-                <button class="btn btn-sm btn-outline-secondary">Share</button>
-                <button class="btn btn-sm btn-outline-secondary">Export</button>
+               <!-- <button class="btn btn-sm btn-outline-secondary">Share</button>
+                <button class="btn btn-sm btn-outline-secondary">Export</button>-->
+                <button class="btn btn-sm btn btn-outline-danger">Supprimer <i class="bi bi-trash"></i></button>
               </div>
-              <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
+            <!--  <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
                 This week
-              </button>
+              </button>-->
             </div>
           </div>
 
-         
+        
         </main>
       </div>
     </div>
    <script>
+ 
        var id=  localStorage.getItem("temporaryVarClicke")
     var url = 'http://localhost:3030/events/event/'+id;
     const xhr = new XMLHttpRequest()
@@ -124,17 +97,49 @@
             res = xhr.response;
             //console.log(res)
             
-           var s =''
+          
            
-            s += '<h1>'+res['titreEvent']+'</h1>'
-           
-         
-             document.getElementById("typeEvents").innerHTML = s
+           var s = '<h1 class="h2">'+res['titreEvent']+'</h1>'
+            document.getElementById("typeEvents").innerHTML = s
+            var setTitre =''
+        
+            setTitre +='<div class="btn " style="width: 100%;margin-top:1em;"> <div class=" d-inline p-2 justify-content-start"> Votre titre : </div><div class="d-inline p-2 justify-content-end">'+res['titreEvent']+'</div><div></div>'
+ 
        
-            
+            document.getElementById("infoTitre").innerHTML =setTitre
+            var setBudget =''
+      
+            setBudget +='<div class="btn "   style="width: 100%; margin-top:1em;"> <div class=" d-inline p-2 justify-content-start"> Votre Budget :</div><div class="d-inline p-2 justify-content-end"> '+res['budgetEvent']+' € </div><div></div>'
+
+            document.getElementById("infoBudget").innerHTML =setBudget
+            var settype =''
+   
+            settype +=' <div class="btn " style="width: 100%; margin-top:1em;"> <div class=" d-inline p-2 justify-content-start">Catégorie : </div><div class="d-inline p-2 justify-content-end">'+res['_typeEvent']["libelle"]+'</div><div></div>'
+  
+            document.getElementById("infoTypeEvent").innerHTML =settype
+            var setDate =''
+       
+            setDate +='<div class="btn " style="width: 100%; margin-top:1em;"> <div class=" d-inline p-2 justify-content-start">Date : </div><div class="d-inline p-2 justify-content-end">'+res['dateEvent']+' </div><div></div>'
+    
+            document.getElementById("infoDate").innerHTML =setDate
+
         }
     };
     xhr.send()
+
+
     </script>
+    <style>
+       .btn {
+        border-width: 2px;
+        border-color: #ffeecc;
+
+}
+    .btn:hover {
+      background-color: #ffeecc;
+
+}
+
+    </style>
 </body>
 </html>
