6e758930e0acc72a45dbe91249eca820979da56a
shirokasunawa <61684977+shirokasunawa@users.noreply.github.com>
2021-07-06 20:39:03 +0200

    checklist prepare ok

--- myEvent.php
+++ myEvent.php
@@ -29,9 +29,11 @@
             </div>
             <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
               <span>Vos Actions</span>
-              <a class="d-flex align-items-center text-muted" href="#">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>
-              </a>
+            
+              <diV class="d-flex align-items-center text-muted" >
+   <svg class="svg" onclick="afficheChecklist()" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>
+               
+              </diV>
             </h6>
             <ul class="nav flex-column mb-2">
               <li class="nav-item">
@@ -69,7 +71,7 @@
               <div class="btn-group mr-2">
                <!-- <button class="btn btn-sm btn-outline-secondary">Share</button>
                 <button class="btn btn-sm btn-outline-secondary">Export</button>-->
-                <button class="btn btn-sm btn btn-outline-danger">Supprimer <i class="bi bi-trash"></i></button>
+                <button class="btn btn-sm btn btn-outline-danger" style="    border-color: #dc3545;">Supprimer <i class="bi bi-trash"></i></button>
               </div>
             <!--  <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
@@ -78,6 +80,8 @@
             </div>
           </div>
 
+          <span id="bodyTabBoard"> </span>
+         
         
         </main>
       </div>
@@ -103,23 +107,23 @@
             document.getElementById("typeEvents").innerHTML = s
             var setTitre =''
         
-            setTitre +='<div class="btn " style="width: 100%;margin-top:1em;"> <div class=" d-inline p-2 justify-content-start"> Votre titre : </div><div class="d-inline p-2 justify-content-end">'+res['titreEvent']+'</div><div></div>'
+            setTitre +='<div class="btn " style="width: 100%;margin-top:1em;" onclick="modifTitre()"> <div class=" d-inline p-2 justify-content-start"> Votre titre : </div><div class="d-inline p-2 justify-content-end">'+res['titreEvent']+'</div><div></div>'
  
        
             document.getElementById("infoTitre").innerHTML =setTitre
             var setBudget =''
       
-            setBudget +='<div class="btn "   style="width: 100%; margin-top:1em;"> <div class=" d-inline p-2 justify-content-start"> Votre Budget :</div><div class="d-inline p-2 justify-content-end"> '+res['budgetEvent']+' € </div><div></div>'
+            setBudget +='<div class="btn "   style="width: 100%; margin-top:1em;" onclick="modifBudget()" > <div class=" d-inline p-2 justify-content-start"> Votre Budget :</div><div class="d-inline p-2 justify-content-end"> '+res['budgetEvent']+' € </div><div></div>'
 
             document.getElementById("infoBudget").innerHTML =setBudget
             var settype =''
    
-            settype +=' <div class="btn " style="width: 100%; margin-top:1em;"> <div class=" d-inline p-2 justify-content-start">Catégorie : </div><div class="d-inline p-2 justify-content-end">'+res['_typeEvent']["libelle"]+'</div><div></div>'
+            settype +=' <div class="btn " style="width: 100%; margin-top:1em;" onclick="modifType()"> <div class=" d-inline p-2 justify-content-start">Catégorie : </div><div class="d-inline p-2 justify-content-end">'+res['_typeEvent']["libelle"]+'</div><div></div>'
   
             document.getElementById("infoTypeEvent").innerHTML =settype
             var setDate =''
        
-            setDate +='<div class="btn " style="width: 100%; margin-top:1em;"> <div class=" d-inline p-2 justify-content-start">Date : </div><div class="d-inline p-2 justify-content-end">'+res['dateEvent']+' </div><div></div>'
+            setDate +='<div class="btn " style="width: 100%; margin-top:1em;" onclick="modifDate()"> <div class=" d-inline p-2 justify-content-start">Date : </div><div class="d-inline p-2 justify-content-end">'+res['dateEvent']+' </div><div></div>'
     
             document.getElementById("infoDate").innerHTML =setDate
 
@@ -127,7 +131,110 @@
     };
     xhr.send()
 
+    function modifTitre(){
+      var setBodyForm=''
+      setBodyForm+=' <div class="d-flex justify-content-center">'
+      setBodyForm+='<div class="form-signin">'
+      setBodyForm+=' <div class="form-group row">'
+      setBodyForm+=' <label for="titreEvent"  class="col-form-label">Donnez un titre à votre évènement</label>'
+      setBodyForm+=' <div class="">'
+      setBodyForm+='  <input type="text"   class="form-control" id="titreEvent" >'
+      setBodyForm+=' </div>'
+      setBodyForm+=' </div></br>'
+      setBodyForm+=' <button class="btn ">Modifier</button></div></div>'
+      document.getElementById("bodyTabBoard").innerHTML = setBodyForm
+    }
 
+    function modifBudget(){
+      var setBodyForm=''
+      setBodyForm+=' <div class="d-flex justify-content-center">'
+      setBodyForm+='<div class="form-signin">'
+      setBodyForm+='<div class="form-group row">'
+      setBodyForm+='  <label for="budgetEvent" class=" col-form-label">Quel est votre budget ?</label>'
+      setBodyForm+='  <div class="">'
+      setBodyForm+=' <input type="number" class="form-control" id="budgetEvent" name="budgetEvent"min="0" >'
+      setBodyForm+=' </div>'
+      setBodyForm+='</div></br>'
+      setBodyForm+=' <button class="btn ">Modifier</button></div></div>'
+      document.getElementById("bodyTabBoard").innerHTML = setBodyForm
+    }
+function modifType(){
+  var url = 'http://localhost:3030/events/typeEvents/';
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', url, true)
+    xhr.setRequestHeader('content-type', 'application/json')
+    xhr.setRequestHeader('authorization', 'Bearer 123abc456def')
+    xhr.responseType = "json"
+    var res
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            res = xhr.response;
+            console.log(res)
+            var setBodyForm=''
+            setBodyForm+=' <div class="d-flex justify-content-center">'
+            setBodyForm+='<div class="form-signin">'
+            setBodyForm+='<div class="form-group row">'
+            setBodyForm +=' <label for="typeProduit" class=" col-form-label">Choississez le type du produit</label>'
+            setBodyForm +='  <div class="">'
+            setBodyForm +='   <select class="form-select" id="typeProduit">'
+            
+            for(var i=0;i<res.length;i++){
+              setBodyForm += '<option value=\''+res[i]["_id"]+'\'>'+res[i]["libelle"]+'</option>'
+            }
+            setBodyForm +='   </select>'
+         
+            setBodyForm+=' </div>'
+            setBodyForm+='</div></br>'
+            setBodyForm+=' <button class="btn ">Modifier</button></div></div>'
+            document.getElementById("bodyTabBoard").innerHTML = setBodyForm
+            
+      
+            
+        }
+    };
+    xhr.send()
+}
+
+function modifDate(){
+          var setBodyForm=''
+          setBodyForm+=' <div class="d-flex justify-content-center">'
+          setBodyForm+='<div class="form-signin">'
+          setBodyForm+='<div class="form-group row">'
+          setBodyForm+=' <label for="dateEvent" class=" col-form-label">Choississez la date de votre évènement</label>'
+          setBodyForm+=' <div class="">'
+          setBodyForm+=' <input type="date" class="form-control" id="dateEvent" name="dateEvent" min="0" >'
+          setBodyForm+='  </div>'
+          setBodyForm+='</div></br>'
+          setBodyForm+=' <button class="btn ">Modifier</button></div></div>'
+          document.getElementById("bodyTabBoard").innerHTML = setBodyForm
+}
+
+function afficheChecklist(){
+  var url = 'http://localhost:3030/events/checklist/';
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', url, true)
+    xhr.setRequestHeader('content-type', 'application/json')
+    xhr.setRequestHeader('authorization', 'Bearer 123abc456def')
+    xhr.responseType = "json"
+    var res
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            res = xhr.response;
+            console.log(res)
+            var setBodyForm=''
+            
+            for(var i=0;i<res.length;i++){
+              setBodyForm += '<div class="d-flex flex-row"><div class="d-flex " ><i style=" font-size:22px !important; padding-top: 15px; " class="bi bi-plus-circle "></i></div><div class="btn d-flex justify-content-center" style="margin-top:1em;margin-bottom:1em;width:100%;text-align: center; margin-left:1em" >  '+res[i]['titreCheclist']+'</div></div>'
+            }
+            
+            document.getElementById("bodyTabBoard").innerHTML = setBodyForm
+            
+      
+            
+        }
+    };
+    xhr.send()
+}
     </script>
     <style>
        .btn {
@@ -139,7 +246,12 @@
       background-color: #ffeecc;
 
 }
+.svg:hover {
+      background-color: #ffeecc;
+      cursor: pointer;
+      border-radius: 15px;
 
+}
     </style>
 </body>
 </html>
