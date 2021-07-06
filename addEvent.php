c01e721bfc073ef1d9cccdd92d0ac0575d7cb500
shirokasunawa <61684977+shirokasunawa@users.noreply.github.com>
2021-07-06 16:25:40 +0200

    add event ok

--- addEvent.php
+++ addEvent.php
@@ -11,12 +11,13 @@
     document.location.href = "index.php"
   }
   </script>
-  <p>Choississez qu'elle genre d'évènement vous voulez crééer</p>
+  <span id="titre"></span>
+  
    
 </br>
    <span id="typeEvents"></span>
    <script>
-     
+     document.getElementById("titre").innerHTML = '<p>Choississez qu\'elle genre d\'évènement vous voulez crééer</p>'
       var url = 'http://localhost:3030/events/typeEvents/';
     const xhr = new XMLHttpRequest()
     xhr.open('GET', url, true)
@@ -31,7 +32,7 @@
             if( Object.keys(res).length!=0){
            var s =''
            for(var i =0;i<res.length;i++){
-            s += '<button type="button" class="btn btn-outline-info" style="width: 18rem;margin: 1.5rem 1.5rem;height:10rem;" onclick="addEvent(\''+res[i]["_id"]+'\')">'+res[i]["libelle"]+'</button>'
+            s += '<button type="button" class="btn btn-outline-info" style="width: 18rem;margin: 1.5rem 1.5rem;height:10rem;" onclick="addInfo(\''+res[i]["_id"]+'\')">'+res[i]["libelle"]+'</button>'
            }
          //console.log(s)
              document.getElementById("typeEvents").innerHTML = s
@@ -40,9 +41,43 @@
         }
     };
     xhr.send()
-       
+  
+    function addInfo(idEvent){
+      document.getElementById("titre").innerHTML = '<p>Ajouter quelques informations</p>'
+      var sForm =''
+      sForm+=' <div class="d-flex justify-content-center">'
+      sForm+='<main class="form-signin">'
+      sForm+=' <div class="form-group row">'
+      sForm+=' <label for="titreEvent"  class="col-form-label">Donnez un titre à votre évènement</label>'
+      sForm+=' <div class="">'
+      sForm+='  <input type="text"   class="form-control" id="titreEvent" >'
+      sForm+=' </div>'
+      sForm+=' </div>'
+      sForm+='<div class="form-group row">'
+      sForm+='  <label for="budgetEvent" class=" col-form-label">Quel est votre budget ?</label>'
+      sForm+='  <div class="">'
+      sForm+=' <input type="number" class="form-control" id="budgetEvent" name="budgetEvent"min="0" >'
+      sForm+=' </div>'
+      sForm+='</div>'
+      sForm+='<div class="form-group row">'
+      sForm+=' <label for="dateEvent" class=" col-form-label">Choississez la date de votre évènement</label>'
+      sForm+=' <div class="">'
+      sForm+=' <input type="date" class="form-control" id="dateEvent" name="dateEvent" min="0" >'
+      sForm+='  </div>'
+      sForm+='</div></br>'
+      sForm+=' <button style="background-color:#e685b5 ;  border-color:#e685b5" class="w-100 btn btn-lg btn-primary"  type="button" onclick="addEvent(\''+idEvent+'\')">Ajouter</button>'
+      sForm+='</main>'
+      sForm+='</div>'
+      document.getElementById("typeEvents").innerHTML =sForm
+    }
   
 function addEvent(idtypeevent){
+  var titreEvent =  document.getElementById("titreEvent").value
+  if(titreEvent == null) {titreEvent="A définir"}
+  var budgetEvent =  document.getElementById("budgetEvent").value
+  if(budgetEvent == null) {budgetEvent=0}
+  var dateEvent =  document.getElementById("dateEvent").value
+  if(dateEvent == null) {dateEvent="A définir"}
   //iduser
   console.log("id event "+idtypeevent)
     var id =  localStorage.getItem("_id")
@@ -50,10 +85,10 @@ function addEvent(idtypeevent){
   
  
     var body = {
-        "titreEvent": "A remplir heyyyy",
+        "titreEvent": titreEvent,
         "_typeEvent": idtypeevent,
-        "dateEvent":"A remplir",
-        "budgetEvent":0
+        "dateEvent":dateEvent,
+        "budgetEvent":budgetEvent
        
         
     };
